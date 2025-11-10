2af4a5e14e2fe556d5c6a6d819f06bc3bc4f0578
Jeskozys <manaadacuy18@gmail.com>
2025-11-10 21:34:11 +0700

    etst

--- resources/views/layout/navbar_users.blade.php
+++ resources/views/layout/navbar_users.blade.php
@@ -4,7 +4,7 @@
             <img src="{{ asset('images/Logo YOT.png') }}" alt="YOT Logo" class="h-[77px] w-auto max-w-[350px]"> 
         </div> <ul class="nav-links flex list-none items-center gap-[25px] m-0 p-0"> 
             <li><a href="#" class="text-black no-underline font-bold transition-colors duration-300 hover:text-yellow-400">About Us</a></li> 
-            <li><a href="#" class="text-black no-underline font-bold transition-colors duration-300 hover:text-yellow-400">Division</a></li> 
+            <li><a href="#" class="text-black no-underline font-bold transition-colors duration-300 hover:text-yellow-400">Divisison</a></li> 
             <li><a href="#" class="text-black no-underline font-bold transition-colors duration-300 hover:text-yellow-400">Event</a></li> 
             <li><a href="#" class="text-black no-underline font-bold transition-colors duration-300 hover:text-yellow-400">Contact Us</a></li> 
             <li><a href="#" class="login-btn bg-[#FFF000] font-bold text-black py-1.5 px-6 rounded-full no-underline transition-all duration-300 hover:bg-opacity/50 hover:text-black">Login</a></li> 
